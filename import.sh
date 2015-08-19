660838b06c2d343fc8a588a343484d17e6163329
Max Lv <max.c.lv@gmail.com>
2015-08-19 15:25:32 +0800

    add libflashlight

--- import.sh
+++ import.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mvn install:install-file -DgroupId=$1 -DartifactId=$2 -Dversion=$3 -Dfile=$4 -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=./
+mvn install:install-file -DgroupId=$1 -DartifactId=$2 -Dversion=$3 -Dfile=$4 -Dpackaging=$5 -DgeneratePom=true -DlocalRepositoryPath=./
